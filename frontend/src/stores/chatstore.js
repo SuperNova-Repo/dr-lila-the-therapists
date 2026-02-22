9946ce3d8f3d6d6414446d48c6def13c776411ab
Kostya Lolinger <Kostya-Lolinger@users.noreply.huggingface.co>
2026-02-22 13:52:27 +0000

    Create chatstore.js

---
+++ frontend/src/stores/chatstore.js
@@ -0,0 +1,24 @@
+import { create } from 'zustand'
+
+export const useChatStore = create((set) => ({
+  chats: [],
+  currentChat: null,
+  messages: [],
+  isLoading: false,
+  
+  setChats: (chats) => set({ chats }),
+  
+  setCurrentChat: (chat) => set({ currentChat: chat }),
+  
+  setMessages: (messages) => set({ messages }),
+  
+  addMessage: (message) => set((state) => ({
+    messages: [...state.messages, message]
+  })),
+  
+  deleteMessage: (messageId) => set((state) => ({
+    messages: state.messages.filter(m => m.id !== messageId)
+  })),
+  
+  setLoading: (isLoading) => set({ isLoading })
+})) 
