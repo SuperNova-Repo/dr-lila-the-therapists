f3d62d306ec3b344c45464d0d5503c42b1ae480f
Kostya Lolinger <Kostya-Lolinger@users.noreply.huggingface.co>
2026-02-22 14:07:59 +0000

    Create layout/layout.jsx

---
+++ frontend/src/components/layout/layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Sidebar from './Sidebar'
+
+function Layout() {
+  return (
+    <div className="h-screen flex flex-col bg-dark-bg">
+      <Header />
+      <div className="flex-1 flex overflow-hidden">
+        <Sidebar />
+        <main className="flex-1 overflow-hidden">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Layout
